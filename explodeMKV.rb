16907c6e431a387009d8e0b3f36c7ad1cd50fa1b
Karthik Periagaram <dekonvoluted@gmail.com>
2015-08-03 14:44:38 -0700

    Allow setting an output directory
    By default, this is the current directory. The output directory must be
    writable by the user.

--- explodeMKV.rb
+++ explodeMKV.rb
@@ -4,15 +4,32 @@
 
 require 'optparse'
 
+options = { output: File.realpath( Dir.pwd ) }
+
 if __FILE__ == $0
     # Parse options
     OptionParser.new do | opts |
         opts.banner = "Usage: #{$0} [OPTIONS] [INPUTS]"
 
+        opts.on( "-o", "--output PATH", "Write output to PATH" ) do | outputPath |
+            begin
+                options[ :output ] = File.realpath outputPath
+            rescue => error
+                puts error.message
+            end
+        end
+
         opts.on( "-h", "--help", "Display this message" ) do
             puts opts
         end
     end.parse!
+
+    # Ensure output directory is writable
+    begin
+        raise "Output path is not writable - #{options[ :output ] }" unless File.writable? options[ :output ]
+    rescue => error
+        puts error.message
+    end
 
     # Process inputs
     ARGV.each do | input |
