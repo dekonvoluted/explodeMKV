0a02cb566c0afaefbd071d5e7e8f9e7e5323b374
Karthik Periagaram <dekonvoluted@gmail.com>
2015-08-03 14:25:06 -0700

    Help message now printed out

--- explodeMKV.rb
+++ explodeMKV.rb
@@ -2,6 +2,20 @@
 
 # Explode an MKV file into constituent streams
 
+require 'optparse'
+
 if __FILE__ == $0
+    # Parse options
+    OptionParser.new do | opts |
+        opts.banner = "Usage: #{$0} [OPTIONS] [INPUTS]"
+
+        opts.on( "-h", "--help", "Display this message" ) do
+            puts opts
+        end
+    end.parse!
+
+    # Process inputs
+    ARGV.each do | input |
+    end
 end
 
