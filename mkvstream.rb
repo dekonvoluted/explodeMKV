fb058e3bec95b15c45ef2022e00197721c69a292
Karthik Periagaram <dekonvoluted@gmail.com>
2015-08-05 20:16:28 -0700

    Added license headers for copying info

--- mkvstream.rb
+++ mkvstream.rb
@@ -1,4 +1,18 @@
-# Streams from an MKV file
+# This file is part of explodeMKV, a ruby script to extract all streams from an MKV file.
+# Copyright (c) 2015 Karthik Periagaram <dekonvoluted@gmail.com>
+
+# ExplodeMKV is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ExplodeMKV is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ExplodeMKV.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'yaml'
 
